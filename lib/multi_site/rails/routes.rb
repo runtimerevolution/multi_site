6d3aaed5be9131a1aa3ed05958bb6b7720cb5d61
Miguel Guinada <mguinada@gmail.com>
2013-03-15 18:59:33 +0000

    MultiSiteConstraints is part of the MultiSite module

--- lib/multi_site/rails/routes.rb
+++ lib/multi_site/rails/routes.rb
@@ -1,22 +1,24 @@
 module ActionDispatch::Routing
   class Mapper
     def multi_site_scope(&block)
-      scope ':multi_site', :constraints => MultiSiteConstraints.new do
+      scope ':multi_site', :constraints => MultiSite::MultiSiteConstraints.new do
         block.call
       end
     end
   end
 end
 
-class MultiSiteConstraints
-  def matches?(request)
-    #TODO: This is not a definitive solution. Devise principal class is configurable,
-    #so we can't be sure if the the routes will start with users/...
-    #Devise settings must be read to know the principal's name
-    return true if defined?(Devise) and [ 'devise/sessions',
-                                          'users/registrations',
-                                          'users/passwords' ].include?(request.path_parameters[:controller])
+module MultiSite
+  class MultiSiteConstraints
+    def matches?(request)
+      #TODO: This is not a definitive solution. Devise principal class is configurable,
+      #so we can't be sure if the the routes will start with users/...
+      #Devise settings must be read to know the principal's name
+      return true if defined?(Devise) and [ 'devise/sessions',
+                                            'users/registrations',
+                                            'users/passwords' ].include?(request.path_parameters[:controller])
 
-    Site.exists?(:url => request.path_parameters[:multi_site])
+      Site.exists?(:url => request.path_parameters[:multi_site])
+    end
   end
 end
