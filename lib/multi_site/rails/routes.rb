a2716beddb9393ba6b55ddd7d99b08c93782d7ca
Miguel Guinada <mguinada@gmail.com>
2013-03-15 18:55:43 +0000

    Revert previous change. Handling Devise special case

--- lib/multi_site/rails/routes.rb
+++ lib/multi_site/rails/routes.rb
@@ -10,6 +10,13 @@ end
 
 class MultiSiteConstraints
   def matches?(request)
-    Site.exists?(:id => request.path_parameters[:multi_site])
+    #TODO: This is not a definitive solution. Devise principal class is configurable,
+    #so we can't be sure if the the routes will start with users/...
+    #Devise settings must be read to know the principal's name
+    return true if defined?(Devise) and [ 'devise/sessions',
+                                          'users/registrations',
+                                          'users/passwords' ].include?(request.path_parameters[:controller])
+
+    Site.exists?(:url => request.path_parameters[:multi_site])
   end
 end
